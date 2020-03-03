14e0d6ac5f6c9c1fa6e20f21b7883bd341e0c21f
Maksym Shokin <maksym.sh@upplabs.com>
2020-03-03 17:17:33 +0200

    'object-types'

--- using-ts.ts
+++ using-ts.ts
@@ -7,7 +7,7 @@ const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1: number, num2: number) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 let number: number;
@@ -15,7 +15,15 @@ let number: number;
 number = 5;
 
 
-button.addEventListener("click", function() {
-    console.log(add(+input1.value, +input2.value));
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
 });
+
+const x: {
+  name: string;
+  age: number
+} = {
+  name: 'Max',
+  age: 30
+};
 
