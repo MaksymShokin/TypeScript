bef17baffa4f7dbaa486eea64707b929f5b8edeb
Maksym Shokin <maksym.sh@upplabs.com>
2020-03-04 13:31:36 +0200

    'union-types'

--- using-ts.ts
+++ using-ts.ts
@@ -53,3 +53,26 @@ enum Teams {ManUtd = 3, Liverpool, Everton}
 const maxRole = Role.USER;
 console.log('role', maxRole);
 
+//any
+
+let y: any;
+
+let anyArray: any[];
+
+//union types
+
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+combine(5, 20);
+combine('Word', 'Letter');
+
+
+
